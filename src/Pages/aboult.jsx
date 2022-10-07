702ed1c3ecd2159efbfda22e13707440d226e5ae
Taíse Jungthon <92680599+TayJung@users.noreply.github.com>
2022-10-07 11:24:06 -0300

    Added About and styled for desktop and mobile responsive

--- src/Pages/aboult.jsx
+++ src/Pages/aboult.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
+import Img from "../Assets/ImageAbout/family.png";
+import * as S from "../Styles/styleAbout";
 
 export default function About() {
-  return <></>;
+  return (
+    <S.Container>
+      <S.Box>
+        <S.Figure>
+          <Tilt
+            tiltMaxAngleX={40}
+            tiltMaxAngleY={40}
+            perspective={900}
+            transitionSpeed={2000}
+            scale={1.1}
+            gyroscope={true}
+          >
+            <S.Image src={Img} alt="pic" title="family" />
+          </Tilt>
+        </S.Figure>
+      </S.Box>
+      <S.BoxTwo>
+        <S.H2>About</S.H2>
+        <S.Article>
+          <S.P>
+            Meu Nome é Taíse mais algumas pessoas também me chamam de Tay tenho
+            32 Anos, sou casada há 13 anos e sou mãe de uma menina de 8 anos.
+            Atualmente escolhi mudar de carreira para Front End que é o que fez
+            meu coração acelerar mais rápido na hora de escolher o meu novo
+            caminho. Sou amante de livros e séries, e adoro passar tempo com
+            minha família e meus animais de estimação.
+          </S.P>
+        </S.Article>
+      </S.BoxTwo>
+    </S.Container>
+  );
 }
---
+++ src/Styles/styleAbout.jsx
@@ -0,0 +1,99 @@
+import Styled from "styled-components";
+import Img from "../Assets/ImageAbout/image.jpg";
+
+export const Container = Styled.section`
+display:flex;
+justify-content:center;
+align-items:center;
+width:100%;
+height:80vh;
+  @media only screen and (min-width: 320px) and (max-width: 500px){
+    flex-direction:column;
+    height:130vh;
+  }
+`;
+
+export const Box = Styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+width:50%;
+height:100%;
+overflow:hidden;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  width:100%;  
+}
+
+`;
+
+export const Image = Styled.img`
+width:15vw;
+height:83vh;
+margin-top:8em;
+transform: translateZ(40px) scale(0.9);
+@media only screen and (min-width: 320px) and (max-width: 500px){
+  width:35vw;
+  height:65vh;
+  margin-top:7em;
+}
+`;
+
+export const Figure = Styled.figure`
+width:90%;
+height:60vh;
+display:flex;
+justify-content:center;
+align-items:center;
+background-image: url(${Img});
+background-size: contain;
+background-repeat: no-repeat;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  width:100%;
+  height:90%;
+}
+`;
+
+export const BoxTwo = Styled.div`
+display:flex;
+justify-content:space-evenly;
+flex-direction:column;
+align-items:center;
+width:50%;
+height:100%;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  width:100%;
+ 
+}
+`;
+
+export const H2 = Styled.h2`
+color:rgba(0, 206, 158, 1);
+margin-bottom:5rem;
+font-size:2em;
+line-height:80%;
+text-transform:uppercase;
+
+-webkit-box-reflect:below 0px linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0,0.4));
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  font-size:1.5em;
+  margin-bottom:0;
+}
+`;
+
+export const Article = Styled.article`
+width:80%;
+height:30vh;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  height:40vh;
+  width:90%;
+ 
+}
+`;
+
+export const P = Styled.p`
+color:#fff;
+font-size:1.1em;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  font-size:0.8em
+}
+`;
