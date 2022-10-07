3169d048729e78861ae69917bd9e1fef17f0210e
Taíse Jungthon <92680599+TayJung@users.noreply.github.com>
2022-10-07 11:02:35 -0300

    Home finished and styled and responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import * as S from "../Styles/styleHome";
 import Typical from "react-typical";
 import TextAnimation from "../Pages/textAnimation";
 import Image from "../Assets/ImageHome/tay.png";
+import { BsInstagram, BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Home = () => {
   return (
@@ -18,10 +19,71 @@ const Home = () => {
         <S.H2>
           <TextAnimation />
         </S.H2>
+        <S.BoxLink>
+          <S.Link
+            href="https://drive.google.com/file/d/1SI4ojJSCKYkJH5oWD2GqHFbcDQp6RkuB/view?usp=sharing"
+            alt="curriculum download button"
+            target="blank"
+          >
+            Download Curriculum
+          </S.Link>
+        </S.BoxLink>
+        <S.BoxRedes>
+          <S.A
+            href="https://www.instagram.com/tay.jung_code/"
+            target="blank"
+            alt="link do instagram"
+            title="Instagram"
+          >
+            <BsInstagram />{" "}
+          </S.A>
+          <S.A
+            href="https://github.com/TayJung"
+            target="blank"
+            alt="link do github"
+            title="Github"
+          >
+            <BsGithub />
+          </S.A>
+          <S.A
+            href="https://www.linkedin.com/in/tayjung29/"
+            target="blank"
+            alt="link do linkedin"
+            title="Linkedin"
+          >
+            <BsLinkedin />
+          </S.A>
+        </S.BoxRedes>
       </S.Box>
-      <>
-        <S.Image src={Image} alt="" />
-      </>
+      <S.BoxTwo>
+        <S.BoxRedesResponsive>
+          <S.A
+            href="https://www.instagram.com/tay.jung_code/"
+            target="blank"
+            alt="link do instagram"
+            title="Instagram"
+          >
+            <BsInstagram />{" "}
+          </S.A>
+          <S.A
+            href="https://github.com/TayJung"
+            target="blank"
+            alt="link do github"
+            title="Github"
+          >
+            <BsGithub />
+          </S.A>
+          <S.A
+            href="https://www.linkedin.com/in/tayjung29/"
+            target="blank"
+            alt="link do linkedin"
+            title="Linkedin"
+          >
+            <BsLinkedin />
+          </S.A>
+        </S.BoxRedesResponsive>
+        <S.Image src={Image} alt="imagem de Taise Jungthon" title="Tay Jung" />
+      </S.BoxTwo>
     </S.Container>
   );
 };
--- src/Styles/styleHome.jsx
+++ src/Styles/styleHome.jsx
@@ -3,20 +3,26 @@ import Styled from "styled-components";
 export const Container = Styled.section`
 width:100%;
 height:80vh;
-
 display:flex;
 justify-content:space-between;
 align-items:center;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  flex-direction:column;
+  height:85vh; 
+}
 `;
 
-export const Box = Styled.div`
+export const Box = Styled.section`
 width:80%;
 height:40vh;
-
 display:flex;
-
 align-items:center;
 flex-direction:column;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  width:100%;
+  height:50vh;
+  justify-content:space-evenly;
+}
 `;
 
 export const Title = Styled.h2`
@@ -24,12 +30,106 @@ color:#fff;
 text-transform:uppercase;
 font-size:2.8em;
 -webkit-text-stroke: 0.3vw #00CE9E;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  font-size:1.3em;
+}
 `;
 
 export const H2 = Styled.h2`
 color:#00CE9E;
 font-size:2em;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  font-size:1em;
+}
 `;
 export const Image = Styled.img`
 width:20vw;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+width:28%;
+}
+`;
+
+export const Link = Styled.a`
+margin-top:1em;
+width:15vw;
+height:7vh;
+border-radius:1em;
+display:flex;
+justify-content:center;
+align-items:center;
+cursor:pointer;
+text-decoration:none;
+color:#fff;
+background:#00CE9E;
+font-weight:bold;
+&:hover{
+  background:#4fed51;;
+  color:#000;
+  border:solid #fff 2px;
+}
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  width:55vw;
+}
+`;
+
+export const BoxLink = Styled.div`
+width:20vw;
+height:15vh;
+display:flex;
+justify-content:center;
+align-items:center;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+
+width:100%;
+}
+`;
+
+export const BoxRedes = Styled.div`
+width:50vw;
+height:60vh;
+display:flex;
+justify-content:center;
+align-items:flex-end;
+@media only screen and (min-width: 320px) and (max-width: 800px){
+  display:none
+}
+`;
+
+export const A = Styled.a`
+text-decoration:none;
+
+>svg{
+  color:#fff;
+  width:2em;
+  font-size:1.5em;
+  &:hover{
+    color:#00CE9E;
+  }
+  @media only screen and (min-width: 320px) and (max-width: 800px){
+
+  }
+}
+`;
+
+export const BoxTwo = Styled.section`
+display:flex;
+align-items:center;
+
+
+ @media only screen and (min-width: 320px) and (max-width: 800px){
+  justify-content:space-between;
+  width:100%;
+  height:40vh;
+ }
+`;
+export const BoxRedesResponsive = Styled.div`
+display:none;
+
+ @media only screen and (min-width: 320px) and (max-width: 800px){
+  height:10vh;
+  width:50vw;
+  display:flex;
+  justify-content:center;
+  align-items:flex-end;
+}
 `;
